08c81d4fd10ea682d40c3c0827954f5e6dbe7b87
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-22 02:01:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string) {
-  console.log (toUpperCase(logShout));
+function logshout(string) {
+  console.log (string.toUpperCase());
 }
