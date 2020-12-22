936067f08772a348899c6f3daf089519e6672304
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-22 02:00:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logshout(string) {
-  console.log (string.toUpperCase());
+function logShout(string) {
+  console.log (toUpperCase(logShout));
 }
